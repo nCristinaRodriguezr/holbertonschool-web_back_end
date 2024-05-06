e906eed08d4d3454f017b55ce017cf03c78c7206
nataly <ncristina.rodriguezr@gmail.com>
2024-05-06 16:53:47 -0500

    :seedling: point 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function add that takes a float a and
+a float b as arguments and returns their sum as a float.
+"""
+
+
+def concat(a: str, b: str) -> str:
+    return a + b
+    
