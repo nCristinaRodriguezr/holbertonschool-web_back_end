f2f6f9a0bf040377391f5c126219b344401ac66e
nataly <ncristina.rodriguezr@gmail.com>
2024-05-17 20:05:01 -0500

    :beers:point 1

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+A function named index_range that takes two
+integer arguments page and page_size
+"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """
+    Devuelve una tupla con el índice de inicio y el índice de fin
+    para paginar una lista de elementos.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Devuelve una lista de filas correspondientes a la página especificada
+        de acuerdo con los argumentos de paginación proporcionados.
+        """
+        assert isinstance(page, int) and page > 0, "Page debe ser un entero mayor que 0"
+        assert isinstance(page_size, int) and page_size > 0, "Page_size debe ser un entero mayor que 0"
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_index >= len(dataset):
+            return []
+
+        return dataset[start_index:end_index]
