9eee4d32ab65e7e43cb66da04d40ab5352b83f37
nataly <ncristina.rodriguezr@gmail.com>
2024-05-25 21:05:02 -0500

    :label:point 1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,8 +1,8 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
   const sizes = [19, 20, 34];
-  const rooms = sizes.map(size => new ClassRoom(size));
+  const rooms = sizes.map((size) => new ClassRoom(size));
   return rooms;
 }
 
