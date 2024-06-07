9a71912981d43f9e7f3924bf31e4297f7f9d71d2
nataly <ncristina.rodriguezr@gmail.com>
2024-06-06 19:48:34 -0500

    :bookmark:point 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,15 @@
 export default function cleanSet(set, startString) {
+  // Verifica si startString está definido y no está vacío
+  if (!startString) {
+    return '';
+  }
+
   const result = [];
   set.forEach((value) => {
     if (value.startsWith(startString)) {
       result.push(value.slice(startString.length));
     }
   });
+
   return result.join('-');
 }
